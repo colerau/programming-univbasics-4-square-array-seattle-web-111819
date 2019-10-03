8260952b16d9885a6345a8099a94bf97705b5f2c
Cole Rau <colevrau@gmail.com>
2019-10-03 23:26:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,8 @@ def square_array(array)
   new_array = []
   while counter < array.length do
     square = array[counter] * array[counter]
-    puts squarenew_array.push(square)
+    puts square
+    new_array.push(square)
     counter += 1
     return new_array
   end
